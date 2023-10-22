9d8eb92fffc68a134c0a0d07fc546569f4140d9f
Deni <denardpeca10@gmail.com>
2023-10-22 12:56:24 +0200

    test

--- src/main/java/com/ecommerce/online/repository/OrderRepository.java
+++ src/main/java/com/ecommerce/online/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package com.ecommerce.online.repository;
 import com.ecommerce.online.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Orders, Long> {
+    Optional<Orders> findById(Long id);
 }
